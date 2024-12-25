6fb1fa553751284c54c91e0a57c39b61c0ae8102
actioncrew <sepeloleksij4@gmail.com>
2024-12-25 10:04:16 +0200

    added player to folder page

--- src/app/components/folder/folder.module.ts
+++ src/app/components/folder/folder.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import { YoutubePlayerComponent } from "../youtube-player/youtube-player.component";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
-  ],
+    FolderPageRoutingModule,
+    YoutubePlayerComponent
+],
   declarations: [FolderPage]
 })
 export class FolderPageModule {}
--- src/app/components/folder/folder.page.ts
+++ src/app/components/folder/folder.page.ts
@@ -10,9 +10,20 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
+  player!: YT.Player;
+  id: string = "qDuKsiwS5xw";
+
   constructor() {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+
+  savePlayer(player: YT.Player) {
+    this.player = player;
+    console.log("player instance", player);
+  }
+  onStateChange(event: YT.PlayerEvent) {
+    console.log("player state", event);
   }
 }
