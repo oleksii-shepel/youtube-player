536ad064d7d10cc1ededb9a2dcd59a11c23081bd
oleksii.shepel@outlook.com <oleksii.shepel@outlook.com>
2025-07-10 13:19:52 +0300

    added drafts for pages

--- src/app/pages/channel/channel.page.ts
+++ src/app/pages/channel/channel.page.ts
@@ -1,28 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { YoutubeDataService } from '../../services/data.service';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { StreamixModule } from 'projects/angular/src/lib';
-import { DirectiveModule } from 'src/app/directives';
-
+import { YoutubePlaylistComponent } from '../../components/youtube-playlist/youtube-playlist.component';
 
 @Component({
-  selector: 'app-search',
+  selector: 'app-channel',
   template: `
-    <div>
-      this works!
-    </div>
-  `,
-  styleUrls: ['./channel.page.scss'],
-  standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    DirectiveModule,
-    StreamixModule
-  ]
-})
-export class ChannelPage {
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button defaultHref="/search"></ion-back-button>
+        </ion-buttons>
+        <ion-title>{{ channel?.snippet.title || 'Channel' }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
 
+    <ion-content *ngIf="channel">
+      <ion-card>
+        <img [src]="channel.snippet.thumbnails.medium.url" />
+        <ion-card-header>
+          <ion-card-title>{{ channel.snippet.title }}</ion-card-title>
+          <ion-card-subtitle>
+            Subscribers: {{ channel.statistics.subscriberCount }}
+          </ion-card-subtitle>
+        </ion-card-header>
+        <ion-card-content>
+          {{ channel.snippet.description }}
+        </ion-card-content>
+      </ion-card>
+
+      <ion-list>
+        <ion-list-header>Playlists</ion-list-header>
+        <app-youtube-playlist
+          *ngFor="let playlist of playlists"
+          [playlistData]="playlist"
+        ></app-youtube-playlist>
+      </ion-list>
+    </ion-content>
+  `,
+  standalone: true,
+  imports: [CommonModule, IonicModule, YoutubePlaylistComponent],
+})
+export class ChannelPage implements OnInit {
+  channelId!: string;
+  channel: any = null;
+  playlists: any[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private youtubeDataService: YoutubeDataService
+  ) {}
+
+  ngOnInit() {
+    this.channelId = this.route.snapshot.paramMap.get('id') || '';
+
+    if (!this.channelId) {
+      this.router.navigate(['/search']);
+      return;
+    }
+
+    this.loadChannel();
+    this.loadPlaylists();
+  }
+
+  loadChannel() {
+    this.youtubeDataService.fetchChannels([this.channelId]).subscribe((res) => {
+      if (res.items.length > 0) {
+        this.channel = res.items[0];
+      }
+    });
+  }
+
+  loadPlaylists() {
+    // Use channelId to fetch playlists owned by channel
+  }
 }
--- src/app/pages/playlist/playlist.page.ts
+++ src/app/pages/playlist/playlist.page.ts
@@ -1,25 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { YoutubeDataService } from '../../services/data.service';
+import { Stream } from '@actioncrew/streamix';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { StreamixModule } from 'projects/angular/src/lib';
-import { DirectiveModule } from 'src/app/directives';
+import { YoutubeVideoComponent } from '../../components/youtube-video/youtube-video.component';
 
 @Component({
-  selector: 'app-search',
+  selector: 'app-playlist',
   template: `
-    <div>this works!</div>
-  `,
-  styleUrls: ['./playlist.page.scss'],
-  standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    DirectiveModule,
-    StreamixModule
-  ]
-})
-export class PlaylistPage {
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button defaultHref="/search"></ion-back-button>
+        </ion-buttons>
+        <ion-title>{{ playlist?.snippet.title || 'Playlist' }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
 
+    <ion-content *ngIf="playlist">
+      <ion-card>
+        <img [src]="playlist.snippet.thumbnails.medium.url" />
+        <ion-card-header>
+          <ion-card-title>{{ playlist.snippet.title }}</ion-card-title>
+          <ion-card-subtitle>
+            Published: {{ playlist.snippet.publishedAt | date }}
+          </ion-card-subtitle>
+        </ion-card-header>
+        <ion-card-content>
+          {{ playlist.snippet.description }}
+        </ion-card-content>
+      </ion-card>
+
+      <ion-list>
+        <ion-list-header>
+          Playlist Videos ({{ videos?.length || 0 }})
+        </ion-list-header>
+        <ion-item *ngFor="let video of videos">
+          <app-youtube-video
+            [videoData]="video"
+            [isCompact]="true"
+          ></app-youtube-video>
+        </ion-item>
+      </ion-list>
+    </ion-content>
+  `,
+  standalone: true,
+  imports: [CommonModule, IonicModule, YoutubeVideoComponent],
+})
+export class PlaylistPage implements OnInit {
+  playlistId!: string;
+  playlist: any = null;
+  videos: any[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private youtubeDataService: YoutubeDataService
+  ) {}
+
+  ngOnInit() {
+    this.playlistId = this.route.snapshot.paramMap.get('id') || '';
+
+    if (!this.playlistId) {
+      this.router.navigate(['/search']);
+      return;
+    }
+
+    this.loadPlaylist();
+    this.loadVideos();
+  }
+
+  loadPlaylist() {
+    this.youtubeDataService
+      .fetchPlaylists([this.playlistId])
+      .subscribe((response) => {
+        if (response.items.length > 0) {
+          this.playlist = response.items[0];
+        }
+      });
+  }
+
+  loadVideos() {
+    // playlistItems API, maxResults=50 for demo (handle pagination as needed)
+  }
 }
