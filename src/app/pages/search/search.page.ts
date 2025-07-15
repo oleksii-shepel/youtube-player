872f55f9e13d0d512abb95627716823c5f663882
oleksii.shepel@outlook.com <oleksii.shepel@outlook.com>
2025-07-15 10:53:51 +0300

    corrections

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -137,8 +137,8 @@ import { RecorderService } from 'src/app/services/recorder.service';
               </ion-button>
 
               <ion-button fill="clear" size="small" (click)="togglePlayer()">
-                <svg *ngIf="!playerHidden$.value" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-off-icon lucide-eye-off"><path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49"/><path d="M14.084 14.158a3 3 0 0 1-4.242-4.242"/><path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143"/><path d="m2 2 20 20"/></svg>
-                <svg *ngIf="playerHidden$.value" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>
+                <svg *ngIf="!playerHidden$.snappy" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-off-icon lucide-eye-off"><path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49"/><path d="M14.084 14.158a3 3 0 0 1-4.242-4.242"/><path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143"/><path d="m2 2 20 20"/></svg>
+                <svg *ngIf="playerHidden$.snappy" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>
               </ion-button>
 
               <ng-container *ngIf="auth$ | async as auth; else loginButton">
@@ -648,10 +648,10 @@ export class SearchPage {
 
 
   togglePlayer() {
-    this.playerHidden$.value ? this.playerService.show() : this.playerService.hide();
+    this.playerHidden$.snappy ? this.playerService.show() : this.playerService.hide();
   }
 
   toggleRecorder() {
-    this.recorderHidden$.value ? this.recorderService.show() : this.recorderService.hide();
+    this.recorderHidden$.snappy ? this.recorderService.show() : this.recorderService.hide();
   }
 }
