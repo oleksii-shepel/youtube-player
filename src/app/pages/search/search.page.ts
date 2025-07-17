f8c209542d92de697e42d62750339c72ef19bf72
oleksii.shepel@outlook.com <oleksii.shepel@outlook.com>
2025-07-17 10:42:30 +0300

    added menu for settings

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,5 +1,5 @@
 import { PlayerService } from './../../services/player.service';
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Stream, Subscription, switchMap } from '@actioncrew/streamix';
 import { debounce, distinctUntilChanged, map } from '@actioncrew/streamix';
 import { GoogleSuggestionsService } from 'src/app/services/suggestions.service';
@@ -141,53 +141,62 @@ import { RecorderService } from 'src/app/services/recorder.service';
                 <svg *ngIf="playerHidden$.snappy" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>
               </ion-button>
 
-              <ng-container *ngIf="auth$ | async as auth; else loginButton">
-                <ion-avatar *ngIf="auth" (click)="openPopover($event)">
-                  <img [src]="auth.profile.picture" />
-                </ion-avatar>
-              </ng-container>
-
-              <ng-template #loginButton>
-                <ion-button
-                  id="google-signin-btn"
-                  fill="clear"
-                  size="small"
-                  class="google-signin-custom"
-                >
-                  <svg class="google-icon" viewBox="0 0 24 24">
-                    <path
-                      fill="#4285f4"
-                      d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-                    />
-                    <path
-                      fill="#34a853"
-                      d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-                    />
-                    <path
-                      fill="#fbbc05"
-                      d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-                    />
-                    <path
-                      fill="#ea4335"
-                      d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
-                    />
-                  </svg>
-                </ion-button>
-              </ng-template>
+              <ion-button fill="clear" size="small" id="settings-button">
+                <ion-icon name="settings-outline"></ion-icon>
+              </ion-button>
 
               <ion-popover
-                [isOpen]="showPopover"
-                [event]="popoverEvent"
-                (didDismiss)="showPopover = false"
+                trigger="settings-button"
+                triggerAction="click"
+                [dismissOnSelect]="true"
+                showBackdrop="false"
                 class="scrollable"
               >
                 <ng-template>
                   <ion-list>
-                    <ion-item (click)="goToProfile()">View Profile</ion-item>
-                    <ion-item (click)="logout()">Logout</ion-item>
+                    <ion-item class="popover-item" button (click)="goToPreferences()" >Preferences</ion-item>
+                    <ion-item class="popover-item" button (click)="reportBug()">Report a Bug</ion-item>
+                    <ion-item class="popover-item" button (click)="sendFeedback()">Send Feedback</ion-item>
+                    <ion-item class="popover-item" button (click)="goToAbout()">About</ion-item>
+                    <ion-item class="popover-item" button *ngIf="auth$ | async as auth; else loginButton" (click)="signOut()">Sign Out</ion-item>
+
+
+                    <ng-template #loginButton>
+                      <ion-item class="popover-item" button (click)="signIn()">
+                        <ion-label>
+                          <div class="google-signin-label">Sign in with Google</div>
+                        </ion-label>
+                        <ion-button
+                          id="google-signin-btn"
+                          fill="clear"
+                          size="small"
+                          class="google-signin-button"
+                        >
+                          <svg class="google-icon" viewBox="0 0 24 24" width="20" height="20">
+                            <path
+                              fill="#4285f4"
+                              d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                            />
+                            <path
+                              fill="#34a853"
+                              d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                            />
+                            <path
+                              fill="#fbbc05"
+                              d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                            />
+                            <path
+                              fill="#ea4335"
+                              d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                            />
+                          </svg>
+                        </ion-button>
+                      </ion-item>
+                    </ng-template>
                   </ion-list>
                 </ng-template>
               </ion-popover>
+
             </div>
           </div>
         </div>
@@ -360,12 +369,18 @@ export class SearchPage {
     this.onSearchQueryChange(event);
   }
 
-  openPopover(event: Event) {
-    this.showPopover = true;
-    this.popoverEvent = event;
+  goToPreferences() {}
+  goToAbout() {}
+  reportBug() {}
+  sendFeedback() {}
+
+  signIn() {
+    this.authorization.signInWithOAuth2();
   }
 
-  goToProfile() {}
+  signOut() {
+    this.authorization.signOut();
+  }
 
   setSort(value: string) {
     this.sortOrder = value;
@@ -388,11 +403,6 @@ export class SearchPage {
   }
 
   onSortOrderChanged() {}
-
-  logout() {
-    this.showPopover = false;
-    this.authorization.signOut();
-  }
 
   toggleMenu() {
     this.playlistService.toggleMenu();
