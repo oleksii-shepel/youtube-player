6450c5988794ee8a5bbe19977a0fd46f74306bc6
oleksii.shepel@outlook.com <oleksii.shepel@outlook.com>
2025-07-18 16:16:55 +0300

    reorganized folder structure

---
+++ api/src/lib/api.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { ApiComponent } from './api.component';
+
+describe('ApiComponent', () => {
+  let component: ApiComponent;
+  let fixture: ComponentFixture<ApiComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [ApiComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ApiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ api/src/lib/api.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-api',
+  templateUrl: './api.component.html',
+  styleUrls: ['./api.component.scss'],
+  standalone: true,
+})
+export class ApiComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
